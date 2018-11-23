5dee81e6c83e622efaaff23abf74152a61d1ea55
Pierre Glaser <pierreglaser@msn.com>
2018-11-23 15:00:34 +0100

    CLN removed save_builtin_function

--- cloudpickle/cloudpickle.py
+++ cloudpickle/cloudpickle.py
@@ -372,13 +372,6 @@ class CloudPickler(Pickler):
 
         return (code, f_globals, defaults, closure, dct, base_globals)
 
-    def save_builtin_function(self, obj):
-        if obj.__module__ == "__builtin__":
-            return self.save_global(obj)
-        return self.save_function(obj)
-
-    dispatch[types.BuiltinFunctionType] = save_builtin_function
-
     def save_global(self, obj, name=None, pack=struct.pack):
         """
         Save a "global".
